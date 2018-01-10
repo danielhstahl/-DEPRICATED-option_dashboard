330ad4f6330d0ab41ae374f98e5a2ceb1cc22869
Daniel Stahl <danstahl1138@gmail.com>
2018-01-09 21:16:35 -0600

    removed unused tests

--- src/Utils/utils.test.js
+++ src/Utils/utils.test.js
@@ -3,20 +3,8 @@ import {
     createArray,
     handleForm,
     removeFirstAndLastElement,
-    filterBasedOffAnotherArray,
-    filterTwoArraysSameFn,
     cartesian
 } from './utils'
-
-it('correctly filters array', ()=>{
-    const arr1=[1, 10, 3, 4]
-    const arr2=[2, 3, 4, 10]
-    const cb=val=>val<10
-    const expected1=[1, 3]
-    const expected2=[2, 4]
-    expect(filterTwoArraysSameFn(arr1, arr2, cb)).toEqual(expected1)
-    expect(filterTwoArraysSameFn(arr2, arr1, cb)).toEqual(expected2)
-})
 
 it('correctly creates array with non-exact parameter', ()=>{
     const init=.001
