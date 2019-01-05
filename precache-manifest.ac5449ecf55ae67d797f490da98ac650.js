10d370ac25b1fb8cd0a2e8f01c73b1df317e0809
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-05 14:03:16 +0000

    Deploy phillyfan1138/option_dashboard to github.com/phillyfan1138/option_dashboard.git:gh-pages

---
+++ precache-manifest.ac5449ecf55ae67d797f490da98ac650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89d3375ece8560a3a520",
+    "url": "/option_dashboard/static/css/main.d763d0c5.chunk.css"
+  },
+  {
+    "revision": "89d3375ece8560a3a520",
+    "url": "/option_dashboard/static/js/main.89d3375e.chunk.js"
+  },
+  {
+    "revision": "d504b23d5adb0921e11b",
+    "url": "/option_dashboard/static/css/1.27329ea1.chunk.css"
+  },
+  {
+    "revision": "d504b23d5adb0921e11b",
+    "url": "/option_dashboard/static/js/1.d504b23d.chunk.js"
+  },
+  {
+    "revision": "de21c69609cfbb574690",
+    "url": "/option_dashboard/static/js/runtime~main.de21c696.js"
+  },
+  {
+    "revision": "1b3134a13e4182301620dbaf200233f6",
+    "url": "/option_dashboard/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/option_dashboard/precache-manifest.ac5449ecf55ae67d797f490da98ac650.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/option_dashboard/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
